d33d6b229f295abb90adeb9864dc60512d011c25
fabiandaviddd <fabianw@icloud.com>
2026-07-23 11:31:49 +0200

    Momente: sw.js

---
+++ sw.js
@@ -0,0 +1,15 @@
+const CACHE = "momente-v1";
+const SHELL = ["./", "index.html", "manifest.webmanifest", "icon-192.png", "icon-512.png", "apple-touch-icon.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(SHELL)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", (e) => {
+  const url = new URL(e.request.url);
+  if (url.origin === location.origin) {
+    e.respondWith(caches.match(e.request).then((hit) => hit || fetch(e.request)));
+  }
+});
